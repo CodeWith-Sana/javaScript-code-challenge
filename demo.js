f22c7cae202954bf1664f96959f8475a92f01a32
Sana-Reactdeveloper24 <anoshakhancs27@gmail.com>
2024-09-26 10:26:24 +0500

    firstrepo

---
+++ demo.js
@@ -0,0 +1,33 @@
+// console.log("Hello World!");
+// let a = "sana";
+// a= 12;
+// console.log(a);
+// console.log(typeof(a))  
+// const object={
+//     "one":123,
+//     "two":"sana"
+// }
+// console.log(object["one"]);
+// object["three"]= 1234; //to add new key value to object;
+// // alos object.four =  value4
+// object.four = "Sana"
+// console.log(object);
+// const dictionary ={
+//     "mango": "fruits" ,
+//     "carrot" : "vegetable" ,
+//     "fish" :"meat",
+//     "apple" : "fruits"
+// }
+
+
+//  var age = 20;
+// if(age > 10 && age <=20)
+//     console.log("your age is in between 10 and 20");
+// // var name = prompt("What is your name?");
+// // var age = prompt("How old are you?");
+
+// if (age > 18) {
+//     alert("Hello " + name + ", you are an adult.");
+// } else {
+//     alert("Hello " + name + ", you are a minor.");
+// }
