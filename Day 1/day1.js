5dbe64ec1b1466238673c73459512dcfece7cc7d
Sana-Reactdeveloper24 <anoshakhancs27@gmail.com>
2024-09-26 12:09:38 +0500

    day1-challenge

---
+++ Day 1/day1.js
@@ -0,0 +1,11 @@
+let name = "Sana"
+
+// function to greet the user with alert messages
+
+function greetUser() {
+    alert("Hello " + name + ", welcome to our website!");
+}
+
+// calling the greetUser function
+
+greetUser();
